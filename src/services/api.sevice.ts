51f78692bb74d11e99d0166dd3ee1e68461291cc
Koranech <antrusnakenko@gmail.com>
2026-03-27 14:30:09 +0200

    hw2/task4 добавив типізацію для response

--- src/services/api.sevice.ts
+++ src/services/api.sevice.ts
@@ -1,9 +1,10 @@
 import type {ProductModel} from "../models/ProductModel.ts";
+import type {ResponseDummyjson} from "../models/ResponseDummyjson.ts";
 
 const productsURL = import.meta.env.VITE_API_URL + '/products'
 
 export const getProducts = async (): Promise<ProductModel[]> => {
-    const response = await fetch(productsURL)
+    const response: ResponseDummyjson = await fetch(productsURL)
         .then(value => value.json())
     return response.products;
 }
