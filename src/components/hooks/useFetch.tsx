ef272e567b933e29fa1d7a672f397c85d1639c78
Koranech <antrusnakenko@gmail.com>
2026-04-27 14:49:23 +0300

    hw6/task2

--- src/components/hooks/useFetch.tsx
+++ src/components/hooks/useFetch.tsx
@@ -1,27 +1,16 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import {objService} from "../../services/api.service.ts";
 
 //В tsx файлах при типізаці стрілочної функції після типу треба писати кому
 export const useFetch = <T, >(defaultvalue: T, url: string, endpoint: string) => {
 
-    const axiosInstance = axios.create({
-        baseURL: url,
-        headers: {}
-    });
-
-     const objService = {
-        getObj: async () => {
-            const {data} = await axiosInstance.get(endpoint);
-            return data;
-        }
-    }
-
     const [obj, setObj] = useState<T>(defaultvalue)
 
     useEffect(() => {
-        objService.getObj()
+        objService.getObj(url, endpoint)
             .then(obj => setObj(obj))
     }, []);
+
 
     return obj;
 };
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_PLACEHOLDER_URL,
     headers: {}
@@ -8,6 +9,20 @@ const axiosInstance = axios.create({
 export const userService = {
     getUsers: async () => {
         const {data} = await axiosInstance.get('/users');
+        return data;
+    }
+}
+
+
+
+export const objService = {
+    getObj: async (url: string, endpoint: string): Promise<any> => {
+        const axiosInstance2 = axios.create({
+            baseURL: url,
+            headers: {}
+        });
+
+        const {data} = await axiosInstance2.get(endpoint);
         return data;
     }
 }
