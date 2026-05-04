cb71f4fa7ac081fdc2b849537df657821a7a26e2
Koranech <antrusnakenko@gmail.com>
2026-05-04 18:42:04 +0300

    hw8/task1 nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import './App.css'
-import LeftBranch from "./components/LeftBranch.tsx";
-import RightBranch from "./components/RightBranch.tsx";
-import {MyContext} from "./context/MyContext.tsx";
-import {useState} from "react";
+import './App.css';
+import {Outlet} from "react-router";
 
+function App() {
 
-const App = () => {
-
-    const [themeColor, setThemeColor] = useState<string>('light')
 
     return (
-        <div>
-            <MyContext.Provider value={{
-                theme: themeColor,
-                changeTheme: (themeValue: string) => {
-                    setThemeColor(themeValue)
-                }
-            }}>
-                <LeftBranch/>
-                <RightBranch/>
-            </MyContext.Provider>
-        </div>
+        <>
+            <Outlet/>
+        </>
     );
-};
+}
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,12 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {routes} from "./router/routes.tsx";
+import {Provider} from "react-redux";
+import {RouterProvider} from "react-router";
+import {routes} from "./routes/router.tsx";
+import {store} from "./redux/store.ts";
 
 
-createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>)
+createRoot(document.getElementById('root')!).render(<Provider store={store}>
+    <RouterProvider router={routes}/>
 
-// createRoot(document.getElementById('root2')!)
-//     .render(
-//         <BrowserRouter>
-//             <Routes>
-//                 <Route path={''} element={<MainLayout/>}>
-//                     <Route index={true} element={<HomePage/>}/>
-//                     <Route path={'users'} element={<UsersPage/>}/>
-//                     <Route path={'posts'} element={<PostsPage/>}/>
-//                     <Route path={'comments'} element={<CommentsPage/>}/>
-//                     <Route path={'products'} element={<ProductsPage/>}/>
-//                 </Route>
-//             </Routes>
-//         </BrowserRouter>
-//     )
+</Provider>);
